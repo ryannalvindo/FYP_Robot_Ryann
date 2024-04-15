03ae8e7bd48b20fb07385df9a53695f6087cacbe
Ryann Alvindo <ryannalvindo@gmail.com>
2024-04-15 18:11:08 +1000

    move the eeprom funcs to library

--- FYP_Arduino/Homing/Homing.ino
+++ FYP_Arduino/Homing/Homing.ino
@@ -22,7 +22,7 @@
 // Define libraries
 #include <TimerInterrupt.h>
 #include <Encoder.h>
-#include <EEPROM.h>
+#include "eeprom_routine.h"
 
 // DEBUG
 #define ENCODER_DEBUG true
@@ -72,11 +72,7 @@ void setup()
 
   Serial.begin(115200);
 
-  // Clear EEPROM
-  for (int i = 0; i < EEPROM.length(); i++)
-  {
-    EEPROM.write(i, 0);
-  }
+  clearEeprom();
 
   // Set motor control pins as outputs
 #if BAILEY_MOTOR_D
@@ -222,25 +218,7 @@ void TimerHandler1()
   oldEncoderValue2 = encoderValue2;
 }
 
-void writingEeprom(long inputNum, long eepromAddress)
-{
-  // Writing the encoder data into EEPROM
-  EEPROM.write(eepromAddress, inputNum & 0xFF);             // Stores 0x38 in address 0
-  EEPROM.write(eepromAddress + 1, (inputNum >> 8) & 0xFF);  // Stores 0x30 in address 1
-  EEPROM.write(eepromAddress + 2, (inputNum >> 16) & 0xFF); // Stores 0x01 in address 2
-  EEPROM.write(eepromAddress + 3, (inputNum >> 24) & 0xFF); // Stores 0x00 in address 3
-}
 
-long readingEeprom(long eepromAddress)
-{
-  // displaying the notification by reading from EEPROM
-  long val = (long)EEPROM.read(eepromAddress) |
-             ((long)EEPROM.read(eepromAddress + 1) << 8) |
-             ((long)EEPROM.read(eepromAddress + 2) << 16) |
-             ((long)EEPROM.read(eepromAddress + 3) << 24);
-
-  return val;
-}
 
 void goToTargetEncoderValue(long targetEncoderValue, uint8_t motor_pin_1, uint8_t motor_pin_2, uint8_t enable_pin)
 {
