f21661318c34e52dc9a1a4ee8ab7532bf8d4f2c0
Ryann Alvindo <ryannalvindo@gmail.com>
2024-04-19 16:51:43 +1000

    Adding feature to control 2 motors

--- FYP_Arduino/Encoder_Targerting_Position/Encoder_Targerting_Position.ino
+++ FYP_Arduino/Encoder_Targerting_Position/Encoder_Targerting_Position.ino
@@ -10,28 +10,32 @@
 #define encoderPinA 2
 #define encoderPinB 4
 
+// Define pins for the encoders (use interrupts pin in one of the signals)
+#define encoderPinA_Motor1 2
+#define encoderPinB_Motor1 4
+#define encoderPinA_Motor2 3
+#define encoderPinB_Motor2 5
 // Define pins for motor control
 #if BAILEY_MOTOR_D
-#define motorPin 5
-#define enablePin 9
+#define motor1Pin 5
+#define enablePin1 9
 #elif RYANN_MOTOR_D
-#define motorPin1 8
-#define motorPin2 9
-#define enablePin 7
+#define motor1Pin1 8
+#define motor1Pin2 9
+#define enablePin1 7
+#define motor2Pin1 10
+#define motor2Pin2 11
+#define enablePin2 12
 #endif
 
 // Target encoder value
 // #define TARGET_ENCODER_VALUE 100
-long TARGET_ENCODER_VALUE = 1000;
+long targetMotor1 = 1000;
+long targetMotor2 = 1000;
 
-// Create an Encoder object
-Encoder myEncoder(encoderPinA, encoderPinB);
-
-// Variables for PID control
-float error, lastError, integral, derivative, output;
-
-// Variables for motor control
-int motorSpeed;
+// Create Encoder objects for both motors
+Encoder encoderMotor1(encoderPinA_Motor1, encoderPinB_Motor1);
+Encoder encoderMotor2(encoderPinA_Motor2, encoderPinB_Motor2);
 
 void setup()
 {
@@ -40,26 +44,27 @@ void setup()
 
 // Set motor control pins as outputs
 #if BAILEY_MOTOR_D
-Serial.println("BAILEY SETTING IS USED");
+  Serial.println("BAILEY SETTING IS USED");
   pinMode(motorPin, OUTPUT);
 #elif RYANN_MOTOR_D
-  pinMode(motorPin1, OUTPUT);
-  pinMode(motorPin2, OUTPUT);
+  pinMode(motor1Pin1, OUTPUT);
+  pinMode(motor1Pin2, OUTPUT);
+  pinMode(motor2Pin1, OUTPUT);
+  pinMode(motor2Pin2, OUTPUT);
 #endif
-  pinMode(enablePin, OUTPUT);
-
-  // Set enablePin to HIGH to enable the motor
-  digitalWrite(enablePin, HIGH);
-}
-
-void loop()
-{
-  // Read the encoder value
-  long encoderValue = myEncoder.read();
+  pinMode(enablePin1, OUTPUT);
+  pinMode(enablePin2, OUTPUT);
 
 #if BAILEY_MOTOR_D
   goToTargetEncoderValue(TARGET_ENCODER_VALUE, motorPin, 0, enablePin);
 #elif RYANN_MOTOR_D
-  goToTargetPos(myEncoder, TARGET_ENCODER_VALUE, motorPin1, motorPin2, enablePin);
+  goToTargetPos(encoderMotor1, targetMotor1, encoderMotor2, targetMotor2, motor1Pin1, motor1Pin2, enablePin1, motor2Pin1, motor2Pin2, enablePin2);
 #endif
+
+
+}
+
+void loop()
+{
+
 }
--- FYP_Arduino/Encoder_Targerting_Position/PID_routine.cpp
+++ FYP_Arduino/Encoder_Targerting_Position/PID_routine.cpp
@@ -1,80 +1,96 @@
 #include <Encoder.h>
 #include "PID_routine.h"
 
-// Define pins for the encoder
-#define encoderPinA 2
-#define encoderPinB 4
-
 double KP = 2.0;
 double KI = 0.0;
 double KD = 0.0;
 
-// Create an Encoder object
-// Encoder encoder1(encoderPinA, encoderPinB);
 
-float pidController(long target, long encoderCount, float *ePrevious, float *eIntegral, unsigned long *previousTime){
-    
-    // Measure the time
-    unsigned long currentTime = micros();
-    float deltaT = ((float)(currentTime - *previousTime))/1.0e6;
+float pidController(long target, Encoder &encoder, float *ePrevious, float *eIntegral, unsigned long *previousTime)
+{
 
-    // Compute the error, eDerivative, and eIntegral
-    int e = target - encoderCount;
-    float eDerivative = (e - *ePrevious) / deltaT;
-    *eIntegral = *eIntegral + (e * deltaT);
-    
-    // Compute the PID control signal
-    float output = (KP * e) + (KD * eDerivative) + (KI * *eIntegral);
+  // Read the encoder value
+  long encoderCount = encoder.read();
 
-    // Update variables for the next iteration
-    *previousTime = currentTime;
-    *ePrevious = e;
+  // Measure the time
+  unsigned long currentTime = micros();
+  float deltaT = ((float)(currentTime - *previousTime)) / 1.0e6;
+  
+  // Compute the error, eDerivative, and eIntegral
+  int e = target - encoderCount;
+  float eDerivative = (e - *ePrevious) / deltaT;
+  *eIntegral = *eIntegral + (e * deltaT);
 
-    return output;
+  // Compute the PID control signal
+  float output = (KP * e) + (KD * eDerivative) + (KI * *eIntegral);
+
+  // Update variables for the next iteration
+  *previousTime = currentTime;
+  *ePrevious = e;
+
+  return output;
 }
 
-void goToTargetPos (Encoder &encoder1, long targetPos, uint8_t motor_pin_1, uint8_t motor_pin_2, uint8_t enable_pin){
+
+// Running the motor
+bool motorRun(int motorSpeed, uint8_t motorPin1, uint8_t motorPin2, uint8_t enablePin)
+{
+  // margin of error is 10
+  if (motorSpeed >= 100)  {
+    digitalWrite(motorPin1, HIGH);
+    digitalWrite(motorPin2, LOW);
+    analogWrite(enablePin, abs(motorSpeed));
+  }
+  else if (motorSpeed <= -100)  {
+    digitalWrite(motorPin1, LOW);
+    digitalWrite(motorPin2, HIGH);
+    analogWrite(enablePin, abs(motorSpeed));
+  }
+  else  {
+    digitalWrite(motorPin1, LOW);
+    digitalWrite(motorPin2, LOW);
+    // Stop the motor
+    analogWrite(enablePin, 0);
+    return 0;
+  }
+
+  return 1;
+}
+
+void goToTargetPos(Encoder &encoder1, long targetPos1, Encoder &encoder2, long targetPos2, uint8_t motor1_pin_1, uint8_t motor1_pin_2, uint8_t enable_pin1, uint8_t motor2_pin_1, uint8_t motor2_pin_2, uint8_t enable_pin2)
+{
   // declare variable
-  float error, lastError, eIntegral, output;
-  unsigned long lastTime = micros();
-  int motorSpeed;  
-  
-  while(true){
-    // Read the encoder value
+  float lastError_1, eIntegral_1, output_1, lastError_2, eIntegral_2, output_2;
+  unsigned long lastTime_1 = micros();
+  unsigned long lastTime_2 = micros();
+  int motorSpeed_1, motorSpeed_2;
+  bool motor1_status = 1;
+  bool motor2_status = 1;
+
+  do
+  {
+    output_1 = pidController(targetPos1, encoder1, &lastError_1, &eIntegral_1, &lastTime_1);
+    output_2 = pidController(targetPos2, encoder2, &lastError_2, &eIntegral_2, &lastTime_2);
+
     
-    long encoderValue = encoder1.read();
 
-    output = pidController(targetPos, encoderValue, &lastError, &eIntegral, &lastTime);
-    
-    motorSpeed = constrain(output, -255, 255);
+    motorSpeed_1 = constrain(output_1, -255, 255);
+    motorSpeed_2 = constrain(output_2, -255, 255);
 
-    if (motorSpeed > 0){
-      digitalWrite(motor_pin_1, HIGH);
-      digitalWrite(motor_pin_2, LOW);
-      analogWrite(enable_pin, abs(motorSpeed));
-    }
-    else if (motorSpeed < 0){
-      digitalWrite(motor_pin_1, LOW);
-      digitalWrite(motor_pin_2, HIGH);
-      analogWrite(enable_pin, abs(motorSpeed));
-    }
-    else{
-      digitalWrite(motor_pin_1, LOW);
-      digitalWrite(motor_pin_2, LOW);
-      // Stop the motor
-      analogWrite(enable_pin, 0);
-      break;
-    }
+    motor1_status = motorRun(motorSpeed_1, motor1_pin_1, motor1_pin_2, enable_pin1);
+    motor2_status = motorRun(motorSpeed_2, motor2_pin_1, motor2_pin_2, enable_pin2);
 
-  Serial.print(targetPos);
-  Serial.print(", ");
-  Serial.println(encoder1.read()); 
+    Serial.print(targetPos1);
+    Serial.print(", ");
+    Serial.println(encoder1.read());
+
+    Serial.print(targetPos2);
+    Serial.print(", ");
+    Serial.println(encoder2.read());
 
     // Add a delay to avoid excessive readings
     delay(100);
-  }
+  }while (motor1_status + motor2_status);
 }
-
-
 
 
