bdfd30916169450ffefa3c0ffff22def38062a43
Ryann Alvindo <ryannalvindo@gmail.com>
2024-05-08 00:26:01 +1000

    update the library

--- FYP_Arduino/ROSArduinoBridge/PID_routine.cpp
+++ FYP_Arduino/ROSArduinoBridge/PID_routine.cpp
@@ -1,23 +1,23 @@
 #include <Encoder.h>
 #include "PID_routine.h"
 
-double KP = 2.0;
-double KI = 0.0;
-double KD = 0.0;
+double KP = 1.005;
+double KI = 0.000;
+double KD = 0 ; //0.25712;
 
 
-float pidController(long target, Encoder &encoder, float *ePrevious, float *eIntegral, unsigned long *previousTime)
+float pidController(long target, long encoderCount, float *ePrevious, float *eIntegral, unsigned long *previousTime)
 {
 
   // Read the encoder value
-  long encoderCount = encoder.read();
+  // long encoderCount = encoder.read();
 
   // Measure the time
   unsigned long currentTime = micros();
   float deltaT = ((float)(currentTime - *previousTime)) / 1.0e6;
   
   // Compute the error, eDerivative, and eIntegral
-  int e = target - encoderCount;
+  long e = target - encoderCount;
   float eDerivative = (e - *ePrevious) / deltaT;
   *eIntegral = *eIntegral + (e * deltaT);
 
@@ -28,67 +28,68 @@ float pidController(long target, Encoder &encoder, float *ePrevious, float *eInt
   *previousTime = currentTime;
   *ePrevious = e;
 
+  //Serial.println(output);
+
   return output;
 }
 
 
 // Running the motor
-bool motorRun(int motorSpeed, uint8_t motorPin1, uint8_t motorPin2, uint8_t enablePin)
+bool motorRun(int motorSpeed, uint8_t motorPin, uint8_t speedPin)
 {
+
   // margin of error is 10
+  // Forward
   if (motorSpeed >= 100)  {
-    digitalWrite(motorPin1, HIGH);
-    digitalWrite(motorPin2, LOW);
-    analogWrite(enablePin, abs(motorSpeed));
+    digitalWrite(motorPin, LOW);         
+    analogWrite(speedPin, abs(motorSpeed));
   }
+  // Backward
   else if (motorSpeed <= -100)  {
-    digitalWrite(motorPin1, LOW);
-    digitalWrite(motorPin2, HIGH);
-    analogWrite(enablePin, abs(motorSpeed));
+    digitalWrite(motorPin, HIGH);        
+    analogWrite(speedPin, abs(motorSpeed));
   }
   else  {
-    digitalWrite(motorPin1, LOW);
-    digitalWrite(motorPin2, LOW);
+    digitalWrite(motorPin, LOW);
     // Stop the motor
-    analogWrite(enablePin, 0);
+    analogWrite(speedPin, 0);
     return 0;
   }
 
   return 1;
 }
 
-void goToTargetPos(Encoder &encoder1, long targetPos1, Encoder &encoder2, long targetPos2, uint8_t motor1_pin_1, uint8_t motor1_pin_2, uint8_t enable_pin1, uint8_t motor2_pin_1, uint8_t motor2_pin_2, uint8_t enable_pin2)
+void goToTargetPos(Encoder &encoder_left, long target_pos_left, Encoder &encoder_right, long target_pos_right, uint8_t motorLeft, uint8_t speedLeft, uint8_t motorRight, uint8_t speedRight)
 {
   // declare variable
-  float lastError_1, eIntegral_1, output_1, lastError_2, eIntegral_2, output_2;
-  unsigned long lastTime_1 = micros();
-  unsigned long lastTime_2 = micros();
-  int motorSpeed_1, motorSpeed_2;
+  float last_error_left, eIntegral_left, output_left, last_error_right, eIntegral_right, output_right;
+  unsigned long last_time_left = micros();
+  unsigned long last_time_right = micros();
+  int motor_speed_left, motor_speed_right;
   bool motor1_status = 1;
   bool motor2_status = 1;
 
   do
   {
-    output_1 = pidController(targetPos1, encoder1, &lastError_1, &eIntegral_1, &lastTime_1);
-    output_2 = pidController(targetPos2, encoder2, &lastError_2, &eIntegral_2, &lastTime_2);
+    output_left = pidController(target_pos_left, encoder_left.read(), &last_error_left, &eIntegral_left, &last_time_left);
+    // output right moves in different rotation and the encoder measures in negative way
+    output_right = pidController(target_pos_right, (encoder_right.read() * -1 ), &last_error_right, &eIntegral_right, &last_time_right);
 
-    motorSpeed_1 = constrain(output_1, -255, 255);
-    motorSpeed_2 = constrain(output_2, -255, 255);
+    motor_speed_left = constrain(output_left, -255, 255);
+    motor_speed_right = constrain(output_right, -255, 255);
 
-    motor1_status = motorRun(motorSpeed_1, motor1_pin_1, motor1_pin_2, enable_pin1);
-    motor2_status = motorRun(motorSpeed_2, motor2_pin_1, motor2_pin_2, enable_pin2);
+    motor1_status = motorRun(motor_speed_left, motorLeft, speedLeft);
+    motor2_status = motorRun(motor_speed_right, motorRight, speedRight);
 
-    Serial.print(targetPos1);
+    Serial.print(target_pos_left);
     Serial.print(", ");
-    Serial.println(encoder1.read());
-
-    Serial.print(targetPos2);
+    Serial.print(encoder_left.read());
+    Serial.print("\t\t\t");
+    Serial.print(target_pos_right);
     Serial.print(", ");
-    Serial.println(encoder2.read());
+    Serial.println((encoder_right.read()*-1));    // adjustment for right wheel
 
     // Add a delay to avoid excessive readings
     delay(100);
   }while (motor1_status + motor2_status);
 }
-
-
